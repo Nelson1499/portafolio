7bc566f68077ed18c93d18d97525967e86a46e0e
Nelson1499 <avalosnelson9919@gmail.com>
2023-07-24 12:10:08 -0300

    Actualiza package.json, configura ESLint y .env

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
-import logo from "./logo.svg";
-import "./App.css";
 import Navbar from "./components/shared/Navbar";
 import { BrowserRouter as Router } from "react-router-dom";
 import Routes from "./routes/routes";
-
+import { UserProvider } from "./provider/UserContext";
 const App = () => {
   return (
     <Router className="min-h-screen h-max pb-1">
-      <Navbar />
-      <Routes />
+      <UserProvider>
+        <Navbar />
+        <Routes />
+      </UserProvider>
     </Router>
   );
 };
