ee00bd69686b6ed31851fbf4d9c88bac1d8f7173
Nelson1499 <avalosnelson9919@gmail.com>
2023-12-31 03:33:45 -0300

    editando texto

--- src/components/container/AboutMeContainer.jsx
+++ src/components/container/AboutMeContainer.jsx
@@ -8,15 +8,17 @@ const AboutMeContainer = () => {
       </div>
       <section className="text-xl w-11/12 md:w-9/12 m-auto my-2 space-y-1.5">
         <p>
-          Soy un desarrollador front-end junior apasionado por crear
-          experiencias web atractivas. Utilizo HTML, CSS, JavaScript, Git y
-          React.
+          Soy un desarrollador full-stack junior apasionado por crear
+          experiencias web atractivas y seguras.
+          Sé construir interfaces funcionales y responsivas, desarrollar y
+          asegurar APIs robustas, y trabajar con bases de datos utilizando
+          consultas SQL.
         </p>
         <p>
-          Para construir interfaces funcionales y responsivas. Me entusiasma
-          aprender nuevas tecnologías y colaborar en proyectos desafiantes. Soy
-          diligente, orientado a soluciones y estoy abierto a recibir
-          retroalimentación para mejorar constantemente mis habilidades.
+           Me entusiasma aprender nuevas tecnologías y colaborar
+          en proyectos desafiantes. Soy diligente, orientado a soluciones y
+          estoy abierto a recibir retroalimentación para mejorar constantemente
+          mis habilidades de programación.
         </p>
         <p>
           Estoy listo para unirme a un equipo dinámico y contribuir en el
